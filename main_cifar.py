7678452665f97cea3a04b03325efbb6d789460a9
RicJM <ricardo.marques@upf.edu>
2021-12-09 17:47:38 +0100

    Remove aggretation of losses, to be able to weight them later
    Signed-off-by: RicJM <ricardo.marques@upf.edu>

--- main_cifar.py
+++ main_cifar.py
@@ -73,8 +73,10 @@ class SemiLoss(object):
     def __call__(self, outputs_x, targets_x, outputs_u, targets_u, epoch, warm_up, lambda_u):
         probs_u = torch.softmax(outputs_u, dim=1)
 
-        Lx = -torch.mean(torch.sum(F.log_softmax(outputs_x, dim=1) * targets_x, dim=1))
-        Lu = torch.mean((probs_u - targets_u) ** 2)
+        # Lx = -torch.mean(torch.sum(F.log_softmax(outputs_x, dim=1) * targets_x, dim=1))
+        # Lu = torch.mean((probs_u - targets_u) ** 2)
+        Lx = torch.sum(F.log_softmax(outputs_x, dim=1) * targets_x, dim=1)
+        Lu = (probs_u - targets_u) ** 2
 
         return Lx, Lu, linear_rampup(epoch, warm_up, lambda_u)
 
