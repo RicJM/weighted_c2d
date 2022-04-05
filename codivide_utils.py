89437cfaf612f306f8b0718e253aa3d725f17668
aldakata <ac.tatjer@gmail.com>
2022-04-05 21:51:58 +0200

    better scatter size

--- codivide_utils.py
+++ codivide_utils.py
@@ -124,7 +124,7 @@ def per_sample_plot(loss, targets, targets_clean, figures_folder, epoch):
     CIFAR100_labels = ['apple', 'aquarium_fish', 'baby', 'bear', 'beaver', 'bed', 'bee', 'beetle', 'bicycle', 'bottle', 'bowl', 'boy', 'bridge', 'bus', 'butterfly', 'camel', 'can', 'castle', 'caterpillar', 'cattle', 'chair', 'chimpanzee', 'clock', 'cloud', 'cockroach', 'couch', 'crab', 'crocodile', 'cup', 'dinosaur', 'dolphin', 'elephant', 'flatfish', 'forest', 'fox', 'girl', 'hamster', 'house', 'kangaroo', 'keyboard', 'lamp', 'lawn_mower', 'leopard', 'lion', 'lizard', 'lobster', 'man', 'maple_tree', 'motorcycle', 'mountain', 'mouse', 'mushroom', 'oak_tree', 'orange', 'orchid', 'otter', 'palm_tree', 'pear', 'pickup_truck', 'pine_tree', 'plain', 'plate', 'poppy', 'porcupine', 'possum', 'rabbit', 'raccoon', 'ray', 'road', 'rocket', 'rose', 'sea', 'seal', 'shark', 'shrew', 'skunk', 'skyscraper', 'snail', 'snake', 'spider', 'squirrel', 'streetcar', 'sunflower', 'sweet_pepper', 'table', 'tank', 'telephone', 'television', 'tiger', 'tractor', 'train', 'trout', 'tulip', 'turtle', 'wardrobe', 'whale', 'willow_tree', 'wolf', 'woman', 'worm']
     dispersion = np.random.rand(len(targets_clean))*0.8
     clean_labels = targets == targets_clean
-    s=0.05
+    s=0.2
     num_classes = max(targets_clean)+1
     readable_labels = CIFAR10_labels if num_classes==10 else CIFAR100_labels
     plt.figure(figsize=(35, 10), dpi=80)
