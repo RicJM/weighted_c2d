ee760c9cc5fb3c8b547524a85b7902395452e2d4
Albert Catalan <albert.catalan@idiada.com>
2022-07-19 10:23:53 +0200

    human readable memmory status

--- main_clothing1M.py
+++ main_clothing1M.py
@@ -191,8 +191,49 @@ def create_model_reg(net="resnet50", num_class=14):
     return model
 
 
+def bytes2human(n, format="%(value).1f %(symbol)s", symbols="customary"):
+    SYMBOLS = {
+        "customary": ("B", "K", "M", "G", "T", "P", "E", "Z", "Y"),
+        "customary_ext": (
+            "byte",
+            "kilo",
+            "mega",
+            "giga",
+            "tera",
+            "peta",
+            "exa",
+            "zetta",
+            "iotta",
+        ),
+        "iec": ("Bi", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi", "Yi"),
+        "iec_ext": (
+            "byte",
+            "kibi",
+            "mebi",
+            "gibi",
+            "tebi",
+            "pebi",
+            "exbi",
+            "zebi",
+            "yobi",
+        ),
+    }
+    n = int(n)
+    if n < 0:
+        raise ValueError("n < 0")
+    symbols = SYMBOLS[symbols]
+    prefix = {}
+    for i, s in enumerate(symbols[1:]):
+        prefix[s] = 1 << (i + 1) * 10
+    for symbol in reversed(symbols[1:]):
+        if n >= prefix[symbol]:
+            value = float(n) / prefix[symbol]
+            return format % locals()
+    return format % dict(symbol=symbols[0], value=n)
+
+
 def CUDA_status(to_print):
-    print(f"{to_print}\n{torch.cuda.memory_allocated(device=None)}")
+    print(f"{to_print}\n{bytes2human(torch.cuda.memory_allocated(device=None))}")
 
 
 def main():
